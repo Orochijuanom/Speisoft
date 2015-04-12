29a92a1ca830bed421e86af2d6d99f03bcc71e16
Andres Diaz <rdiaz13@itfip.edu.co>
2015-04-12 02:00:57 -0500

    dwd
    w

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,10 +23,10 @@
 
 					<div class="col-xs-6 col-md-4"><a href="/proveedores"><img src="http://www.speisoft.victorushero.com/iconos/proveedores.png" /></a><h4>Proveedores</h4></a></div>
 					
-					<div class="col-xs-6 col-md-4"><a href="//tipo_productos"><img src="http://www.speisoft.victorushero.com/iconos/tipo_productos.png" /></a><h4>Tipo de Productos</h4></a></div>
+					<div class="col-xs-6 col-md-4"><a href="/tipo_productos"><img src="http://www.speisoft.victorushero.com/iconos/tipo_productos.png" /></a><h4>Tipo de Productos</h4></a></div>
 
 					<div class="col-xs-6 col-md-4"><a href="//productos"><img src="http://www.speisoft.victorushero.com/iconos/productos.png" /></a><h4>Productos</h4></a></div>
-										
+
 				</div>
 			</div>
 		</div>
